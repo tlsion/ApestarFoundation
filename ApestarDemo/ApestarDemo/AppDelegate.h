87bee09f35ae47f93bb7a28ba8adbadeed87d2d4
Tlsion <249190182@qq.com>
2017-01-12 12:58:00 +0800

    0

---
+++ ApestarDemo/ApestarDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ApestarDemo
+//
+//  Created by Eton on 2017/1/6.
+//  Copyright © 2017年 ApeStar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
